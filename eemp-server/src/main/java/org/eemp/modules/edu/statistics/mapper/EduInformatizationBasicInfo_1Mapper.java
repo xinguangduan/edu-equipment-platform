8fda33415cb0d09541d571fe789045f82411453a
welldoer <guomei201102@126.com>
2023-06-29 17:05:48 +0800

    feat: add findNewestYearlyRecord function
    1、参考 SysLogMapper 提供后台数据访问功能
    2、利用 Mybatis-Plus 进行相应的单元测试

--- eemp-server/src/main/java/org/eemp/modules/edu/statistics/mapper/EduInformatizationBasicInfo_1Mapper.java
+++ eemp-server/src/main/java/org/eemp/modules/edu/statistics/mapper/EduInformatizationBasicInfo_1Mapper.java
@@ -1,7 +1,12 @@
 package org.eemp.modules.edu.statistics.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.eemp.modules.edu.statistics.entity.EduInformatizationBasicInfo_1;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Description: 教育信息化基本情况统计表（一）
@@ -9,5 +14,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @Version: V1.0
  */
 public interface EduInformatizationBasicInfo_1Mapper extends BaseMapper<EduInformatizationBasicInfo_1> {
+
+    List<Map<String,Object>> findNewestYearlyRecord(@Param("dayStart") Date dayStart, @Param("dayEnd") Date dayEnd, @Param("dbType") String dbType);
 
 }
