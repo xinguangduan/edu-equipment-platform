58c1a2f51c9ce9122419fc7385b07d2b32a37022
welldoer <guomei201102@126.com>
2023-07-07 13:35:35 +0800

    feat: add packages for edu_informatization_basic_info_2

---
+++ eemp-server/src/main/java/org/eemp/modules/edu/statistics/mapper/EduInformatizationBasicInfo_2Mapper.java
@@ -0,0 +1,13 @@
+package org.eemp.modules.edu.statistics.mapper;
+
+import org.eemp.modules.edu.statistics.entity.EduInformatizationBasicInfo_2;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 教育信息化基本情况统计表（二）
+ * @Date:   2023-07-07
+ * @Version: V1.0
+ */
+public interface EduInformatizationBasicInfo_2Mapper extends BaseMapper<EduInformatizationBasicInfo_2> {
+
+}
---
+++ eemp-server/src/main/java/org/eemp/modules/edu/statistics/service/impl/EduInformatizationBasicInfo_2ServiceImpl.java
@@ -0,0 +1,18 @@
+package org.eemp.modules.edu.statistics.service.impl;
+
+import org.eemp.modules.edu.statistics.entity.EduInformatizationBasicInfo_2;
+import org.eemp.modules.edu.statistics.mapper.EduInformatizationBasicInfo_2Mapper;
+import org.eemp.modules.edu.statistics.service.IEduInformatizationBasicInfo_2Service;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 教育信息化基本情况统计表（二）
+ * @Date:   2023-07-07
+ * @Version: V1.0
+ */
+@Service
+public class EduInformatizationBasicInfo_2ServiceImpl extends ServiceImpl<EduInformatizationBasicInfo_2Mapper, EduInformatizationBasicInfo_2> implements IEduInformatizationBasicInfo_2Service {
+
+}
