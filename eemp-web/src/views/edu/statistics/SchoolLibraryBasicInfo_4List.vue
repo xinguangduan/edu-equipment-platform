72e0e9cd7c4bed08e9337c0f119b075c2e4f0aad
welldoer <guomei201102@126.com>
2023-09-28 21:38:09 +0800

    feat: update vui ui to support add,delete,report,revoke

--- eemp-web/src/views/edu/statistics/SchoolLibraryBasicInfo_4List.vue
+++ eemp-web/src/views/edu/statistics/SchoolLibraryBasicInfo_4List.vue
@@ -4,9 +4,9 @@
    <BasicTable @register="registerTable" :rowSelection="rowSelection">
      <!--插槽:table标题-->
       <template #tableTitle>
-          <a-button type="primary" @click="handleAdd" preIcon="ant-design:plus-outlined"> 新增</a-button>
-          <a-button  type="primary" preIcon="ant-design:export-outlined" @click="onExportXls"> 导出</a-button>
-          <j-upload-button  type="primary" preIcon="ant-design:import-outlined" @click="onImportXls">导入</j-upload-button>
+          <a-button type="primary" @click="handleAdd" preIcon="ant-design:plus-outlined" v-auth="'edu.statistics:school_library_basic_info_4:add'" :disabled="!addable"> 新增</a-button>
+          <a-button  type="primary" preIcon="ant-design:export-outlined" @click="onExportXls" v-auth="'edu.statistics:school_library_basic_info_4:exportXls'"> 导出</a-button>
+          <j-upload-button  type="primary" preIcon="ant-design:import-outlined" @click="onImportXls" v-auth="'edu.statistics:school_library_basic_info_4:importExcel'">导入</j-upload-button>
           <a-dropdown v-if="selectedRowKeys.length > 0">
               <template #overlay>
                 <a-menu>
@@ -16,14 +16,16 @@
                   </a-menu-item>
                 </a-menu>
               </template>
-              <a-button>批量操作
+              <a-button v-auth="'edu.statistics:school_library_basic_info_4:deleteBatch'">批量操作
                 <Icon icon="mdi:chevron-down"></Icon>
               </a-button>
         </a-dropdown>
+        <a-button type="primary" @click="handleReport" preIcon="ant-design:send-outlined" v-auth="'edu.statistics:school_library_basic_info_4:report'" :disabled="!reportable">上报</a-button>
+        <a-button :ghost="true" type="primary" @click="handleRevoke" preIcon="ant-design:send-outlined" v-auth="'edu.statistics:school_library_basic_info_4:revoke'">退回学校修改</a-button>
       </template>
        <!--操作栏-->
       <template #action="{ record }">
-        <TableAction :actions="getTableAction(record)" :dropDownActions="getDropDownAction(record)"/>
+        <TableAction v-if="recId !== undefined && record.id === recId.valueOf() && reportable" :actions="getTableAction(record)" :dropDownActions="getDropDownAction(record)"/>
       </template>
       <!--字段回显插槽-->
       <template #htmlSlot="{text}">
@@ -44,15 +46,20 @@
 </template>
 
 <script lang="ts" name="org.eemp.modules.edu.statistics-schoolLibraryBasicInfo_4" setup>
-  import {ref, computed, unref} from 'vue';
+  import {ref, computed, unref, onMounted} from 'vue';
   import {BasicTable, useTable, TableAction} from '/@/components/Table';
   import {useModal} from '/@/components/Modal';
   import { useListPage } from '/@/hooks/system/useListPage'
   import SchoolLibraryBasicInfo_4Modal from './components/SchoolLibraryBasicInfo_4Modal.vue'
   import {columns, searchFormSchema} from './SchoolLibraryBasicInfo_4.data';
-  import {list, deleteOne, batchDelete, getImportUrl,getExportUrl} from './SchoolLibraryBasicInfo_4.api';
+  import {list, deleteOne, batchDelete, getImportUrl,getExportUrl, reportOne, batchRevoke, getFillingControlUrl} from './SchoolLibraryBasicInfo_4.api';
   import { downloadFile } from '/@/utils/common/renderUtils';
+  import { defHttp } from '/@/utils/http/axios';
+  import { useUserStoreWithOut } from '/@/store/modules/user';
   const checkedKeys = ref<Array<string | number>>([]);
+
+  const userStore = useUserStoreWithOut();
+
   //注册model
   const [registerModal, {openModal}] = useModal();
   //注册table数据
@@ -74,7 +81,7 @@
             },
            actionColumn: {
                width: 120,
-               fixed:'right'
+               fixed:'left'
             },
       },
        exportConfig: {
@@ -134,7 +141,7 @@
     * 成功回调
     */
   function handleSuccess() {
-      (selectedRowKeys.value = []) && reload();
+      (selectedRowKeys.value = []) && getFillingControl() && reload();
    }
    /**
       * 操作栏
@@ -144,17 +151,6 @@
          {
            label: '编辑',
            onClick: handleEdit.bind(null, record),
-         }
-       ]
-   }
-     /**
-        * 下拉操作栏
-        */
-  function getDropDownAction(record){
-       return [
-         {
-           label: '详情',
-           onClick: handleDetail.bind(null, record),
          }, {
            label: '删除',
            popConfirm: {
@@ -164,7 +160,45 @@
          }
        ]
    }
+     /**
+        * 下拉操作栏
+        */
+  function getDropDownAction(record){
+       return [
+        //  {
+        //    label: '详情',
+        //    onClick: handleDetail.bind(null, record),
+        //  }
+       ]
+   }
 
+  async function handleReport() {
+    await reportOne({identificationCode: userStore.getUserInfo.telephone, id: recId.value}, handleSuccess);
+  }
+  async function handleRevoke() {
+    await batchRevoke({ids: selectedRowKeys.value}, handleSuccess);
+  }
+ 
+  const addable = ref(false)
+  const reportable = ref(false)
+  const revokable = ref(false)
+  const recId = ref(0)
+
+  onMounted(() => {
+    getFillingControl()
+    console.log("onMounted...")
+  })
+
+  async function getFillingControl() {
+    let params = {identificationCode: userStore.getUserInfo.telephone, packageName: 'school_library_basic_info_4'};
+    await defHttp.post({url: getFillingControlUrl, params}, {joinParamsToUrl: true}).then((res) => {
+      console.log(res);
+      addable.value = res.addable
+      reportable.value = res.reportable
+      revokable.value = res.revokable
+      recId.value = res.id
+    });
+  }
 
 </script>
 
