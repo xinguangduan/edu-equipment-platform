f41a2944b465e9c059cc77997050f0c2ffe8f24d
welldoer <guomei201102@126.com>
2023-11-29 16:31:05 +0800

    fix: disable choosing school

--- eemp-web/src/views/edu/statistics/EduInformatizationBasicInfo_2.data.ts
+++ eemp-web/src/views/edu/statistics/EduInformatizationBasicInfo_2.data.ts
@@ -132,6 +132,7 @@ export const formSchema: FormSchema[] = [
                  { required: true, message: '请选择学校名称!'},
           ];
      },
+    dynamicDisabled:true,
   },
   {
     label: '填报日期',
